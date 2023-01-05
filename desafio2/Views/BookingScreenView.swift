ec35c194c608b19e11c886ea500257d01596bdab
unicred <unicred@Lucas-MacBook-Pro.local>
2023-01-05 13:29:49 -0300

    features/ new page

---
+++ desafio2/Views/BookingScreenView.swift
@@ -0,0 +1,8 @@
+//
+//  BookingScreenView.swift
+//  desafio2
+//
+//  Created by unicred on 04/01/23.
+//
+
+import Foundation
---
+++ desafio2/Views/HomeScreenView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeScreenView.swift
+//  desafio2
+//
+//  Created by unicred on 04/01/23.
+//
+
+import Foundation
